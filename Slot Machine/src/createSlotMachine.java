453e11740b7b47ba55d487c69db29d46e12dfef2
Jackson.Paul <Jackson.Paul@MU-C1-12.mullen.pvt.k12.co.us>
2017-09-26 14:57:55 -0600

    created slots

--- Slot Machine/src/createSlotMachine.java
+++ Slot Machine/src/createSlotMachine.java
@@ -1,15 +1,17 @@
 import java.util.*;
 public class createSlotMachine
 	{
+		static int col;
+		static Symbol [][] machine = new Symbol[3][3];
 		static ArrayList <Symbol> symbols = new ArrayList <Symbol>();
 		public static void main(String[] args)
 			{
 			
 			addSymbols();
-			generateRows();
-			
+			pullLever();
 
 			}
+
 		private static void addSymbols()
 			{
 			
@@ -23,13 +25,111 @@ public class createSlotMachine
 				symbols.add(new Symbol("boomerang", 750, 1.5, false, 1));
 				symbols.add(new Symbol("flag", 50, 12.5, false, 1));
 				symbols.add(new Symbol("aglet", 5000, .1, false, 1));
-				symbols.add(new Symbol("aircraft carrier", 350, 3.6, false, 1));
+				symbols.add(new Symbol("aircraft carrier", 350, 2.6, false, 1));
 				
 			}
-		private static void generateRows()
+		
+		private static void pullLever()
 			{
-			
-				
+			System.out.println("Spinning");
+			Timer dot = new Timer();
+			for (int row = 0; row < 3; row++)
+				{
+				double spin = (double)(Math.random()*100.0 + .1);
+				if (spin < 22.5)
+					{
+						machine [row][col] = symbols.get(0);
+					}
+				else if (spin >=22.5 && spin < 62.5)
+					{
+						machine [row][col] = symbols.get(1);
+					}
+				else if (spin >= 62.5 && spin < 74.5)
+					{
+						machine [row][col] = symbols.get(2);
+					}
+				else if (spin >= 74.5 && spin < 79.5)
+					{
+						machine [row][col] = symbols.get(3);
+					}
+				else if (spin >= 79.5 && spin < 82.0)
+					{
+						machine [row][col] = symbols.get(4);
+					}
+				else if (spin >= 82.0 && spin < 83.0)
+					{
+						machine [row][col] = symbols.get(5);
+					}
+				else if (spin >= 83.0 && spin < 83.5)
+					{
+						machine [row][col] = symbols.get(6);
+					}
+				else if (spin >= 83.5 && spin < 85.0)
+					{
+						machine [row][col] = symbols.get(7);
+					}
+				else if (spin >= 85.0 && spin < 97.5)
+					{
+						machine [row][col] = symbols.get(8);
+					}
+				else if (spin >= 97.5 && spin < 97.6)
+					{
+						machine [row][col] = symbols.get(9);
+					}
+				else
+					{
+						machine [row][col] = symbols.get(10);
+					}
+				for (col = 0; col < 3; col++)
+					{
+						spin = (double)(Math.random()*100.0 + .1);
+						if (spin < 22.5)
+							{
+								machine [row][col] = symbols.get(0);
+							}
+						else if (spin >=22.5 && spin < 62.5)
+							{
+								machine [row][col] = symbols.get(1);
+							}
+						else if (spin >= 62.5 && spin < 74.5)
+							{
+								machine [row][col] = symbols.get(2);
+							}
+						else if (spin >= 74.5 && spin < 79.5)
+							{
+								machine [row][col] = symbols.get(3);
+							}
+						else if (spin >= 79.5 && spin < 82.0)
+							{
+								machine [row][col] = symbols.get(4);
+							}
+						else if (spin >= 82.0 && spin < 83.0)
+							{
+								machine [row][col] = symbols.get(5);
+							}
+						else if (spin >= 83.0 && spin < 83.5)
+							{
+								machine [row][col] = symbols.get(6);
+							}
+						else if (spin >= 83.5 && spin < 85.0)
+							{
+								machine [row][col] = symbols.get(7);
+							}
+						else if (spin >= 85.0 && spin < 97.5)
+							{
+								machine [row][col] = symbols.get(8);
+							}
+						else if (spin >= 97.5 && spin < 97.6)
+							{
+								machine [row][col] = symbols.get(9);
+							}
+						else
+							{
+								machine [row][col] = symbols.get(10);
+							}
+					}
+				}
 			}
+		
 
 	}
